d127f5f74fc8381a9a603a66903fe7c64daf767c
Cale McCollough <you@example.com>
2019-09-27 21:34:22 -0700

    Changes.Add missing script2 files. #2

---
+++ _main.cpp
@@ -0,0 +1,16 @@
+/* Kabuki Database @version 0.x
+@link    https://github.com/kabuki-starship/kabuki.db.git
+@file    /_main.cpp
+@author  Cale McCollough <https://calemccollough.github.io>
+@license Copyright (C) 2014-9 Cale McCollough; all right reserved (R). 
+This Source Code Form is subject to the terms of the Mozilla Public License, 
+v. 2.0. If a copy of the MPL was not distributed with this file, You can 
+obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include <_config.h>
+
+int main () {
+  system("cls");
+  system("title Kabuki.DB server.");
+  system("colr 0f");
+}
